91c9795204add8430ecae0872e1de92c89d5d679
changruill <32725071+changruill@users.noreply.github.com>
2018-11-27 12:02:21 +0800

    k个一组翻转链表

---
+++ ListNode.java
@@ -0,0 +1,141 @@
+public class ListNode {
+
+    Node head = null;
+
+    public static class Node {
+        int val;
+        Node next;
+
+        Node(int val)
+        {
+            this.val = val;
+        }
+    }
+
+    public void addNode(int val)
+    {
+        Node node = new Node(val);
+
+        if(head == null)
+        {
+            head = node;
+            return;
+        }
+        Node current = head;
+        while(current.next != null)
+            current = current.next;
+        current.next = node;
+    }
+
+    public ListNode createList(int size)
+    {
+        ListNode list = new ListNode();
+        for(int i=0; i<size ;i++)
+            list.addNode(i+1);
+        return list;
+    }
+
+    public void printListNode(ListNode list)
+    {
+        ListNode.Node current = list.head;
+
+        while(current.next != null)
+        {
+            System.out.print(current.val + "->");
+            current = current.next;
+        }
+        System.out.println(current.val);
+    }
+
+    public static void reverseNodeInKGroup(ListNode list, int k)
+    {
+        /**
+         * 每k个节点单独划为一组进行处理
+         * 每组内进行一次链表反转
+         * 必须对ListNode.head进行修改
+         * 每组的第一个节点接下一组的最后一个节点
+         *
+         * example: 1-2-3-4-5-6-7-8-9-10-11, k=3
+         * res:     3-2-1-6-5-4-9-8-7-10-11
+         */
+        if(k == 1 || list.head == null || list.head.next == null)
+            return;
+
+        boolean have_changed_head = false;
+        int node_num = 0;
+        int length = 0;
+
+        ListNode.Node current = list.head;
+        // group_head记录的是将要处理的每一组的第一个节点
+        ListNode.Node group_head = list.head;
+        // pre_group_head记录的是上一组尚未处理next的最后一个节点
+        ListNode.Node pre_group_head = null;
+        // next_group_head记录的是下一组尚未处理的第一个节点
+        ListNode.Node next_group_head = null;
+
+        while(current != null)
+        {
+            length++;
+            node_num++;
+
+            if(node_num % k == 0)
+            {
+                next_group_head = current.next;
+                node_num = 0;
+                // 链表翻转算法
+                ListNode.Node prev = group_head;
+                ListNode.Node cur = group_head.next;
+
+                int i = 2;
+                while(cur != null && i <= k)
+                {
+                    ListNode.Node temp = cur.next;
+                    cur.next = prev;
+                    prev = cur;
+                    cur = temp;
+                    i++;
+                }
+                if(!have_changed_head)
+                {
+                    list.head = prev;
+                    have_changed_head = true;
+                }
+                // 每组的第一个节点接下一组的最后一个节点
+                if(pre_group_head == null)
+                    pre_group_head = group_head;
+                else
+                {
+                    pre_group_head.next = prev;
+                    pre_group_head = group_head;
+                }
+                group_head = next_group_head;
+                current = next_group_head;
+            }
+            else
+                current = current.next;
+        }
+
+        // 处理不是整倍数剩下的节点
+        if(length < k)
+            return;
+        else if(length % k == 0)
+        {
+            pre_group_head.next = null;
+            return;
+        }
+        pre_group_head.next = group_head;
+    }
+
+    public static void main(String[] args)
+    {
+        int size = 13;
+        int k = 5;
+
+        ListNode list = new ListNode();
+        list = list.createList(size);
+        list.printListNode(list);
+
+        reverseNodeInKGroup(list, k);
+        list.printListNode(list);
+    }
+}
